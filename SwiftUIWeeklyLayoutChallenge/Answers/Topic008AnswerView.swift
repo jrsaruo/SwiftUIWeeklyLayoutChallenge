8943f275d2c40e186aaf16d19ec6700560e8f0a1
Yusaku Nishi <jrsaruo@gmail.com>
2022-09-21 20:33:39 +0900

    add: Topic008.Application

--- SwiftUIWeeklyLayoutChallenge/Answers/Topic008AnswerView.swift
+++ SwiftUIWeeklyLayoutChallenge/Answers/Topic008AnswerView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+enum Topic008 {
+    
+    struct Application: Identifiable {
+        let id = UUID()
+        let thumbnail: Color
+        let name: String
+        let subtitle: String
+    }
+}
+
 /// <doc:Topic008>
 public struct Topic008AnswerView: View {
     public init() {}
