5b5e44af30c7a89fe09c12af0798eb2021a4c08c
Yusaku Nishi <jrsaruo@gmail.com>
2022-09-21 20:26:44 +0900

    add: Topic008AnswerView

---
+++ SwiftUIWeeklyLayoutChallenge/Answers/Topic008AnswerView.swift
@@ -0,0 +1,23 @@
+//
+//  Topic008AnswerView.swift
+//  
+//
+//  Created by Yusaku Nishi on 2022/09/21.
+//
+
+import SwiftUI
+
+/// <doc:Topic008>
+public struct Topic008AnswerView: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Code your layout here!")
+    }
+}
+
+struct Topic008AnswerView_Previews: PreviewProvider {
+    static var previews: some View {
+        Topic008AnswerView()
+    }
+}
